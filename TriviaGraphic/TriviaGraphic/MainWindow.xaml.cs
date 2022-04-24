fe37385130c10d7e113357c6bf1c8426fc1bf931
nativ kolisher <nativk9@gmail.com>
2022-04-24 23:26:59 +0300

    Changed the theme of the opening screen, added grids and Buttons (remains only the photos for the opening screen) - without actions

--- TriviaGraphic/TriviaGraphic/MainWindow.xaml.cs
+++ TriviaGraphic/TriviaGraphic/MainWindow.xaml.cs
@@ -23,7 +23,16 @@ namespace TriviaGraphic
         public MainWindow()
         {
             InitializeComponent();
-            this.First.FontSize = 14;
+        }
+
+        private void TextBox_TextChanged()
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am circle button");
         }
     }
 }
