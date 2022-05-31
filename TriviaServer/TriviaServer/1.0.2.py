770233539ed8e4a0b84ebd0ab94c86407a69c62b
Ido <idoshoham555@gmail.com>
2022-05-31 18:50:49 +0300

    python bot of 1.0.2

---
+++ TriviaServer/TriviaServer/1.0.2.py
@@ -0,0 +1,25 @@
+import socket
+
+# Create a socket object
+s = socket.socket()
+
+# Define the port on which you want to connect
+port = 8826
+
+# connect to the server on local computer
+s.connect(('127.0.0.1', port))
+
+s.sendall(b'hello')
+# receive data from the server and decoding to get the string.
+print(s.recv(1024).decode())
+
+# login:
+s.sendall(b'21000{"username": "user1", "password": "1234"}')
+print(s.recv(1024).decode())
+
+# signup:
+s.sendall(b'11000{"username": "user1", "password": "1234", "email": "user1@gmail.com"}')
+print(s.recv(1024).decode())
+
+# close the connection
+s.close()
